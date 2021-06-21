47180f61e89d473de10cb6157cce980611726e8d
finale225 <86174673+finale225@users.noreply.github.com>
2021-06-22 01:53:19 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/*#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
@@ -7,28 +7,4 @@ int main() {
     if (a <= 0 || b >= 10) return 0;
     sum = a + b;
     cout << sum;
-}*/
-
-/*#include <iostream>
-using namespace std;
-
-int main() {
-    int a, b, mul = 1;
-    cin >> a >> b;
-    if (a <= 0 || b >= 10) return 0;
-    mul = a * b;
-    cout << mul;
-}*/
-
-#include <iostream>
-using namespace std;
-
-int main() {
-    double a, b;
-    double div = 1;
-    cin >> a >> b;
-    if (a <=0 || b >= 10 || b == 0) return 0;
-    div = a / b;
-    cout.precision(30);
-    cout << div;
 }
