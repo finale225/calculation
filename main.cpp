85cb6c578b3ca3208d651a7963b5276e3c033d72
finale225 <finale2255@naver.com>
2021-06-22 01:50:59 +0900

    MUL

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 int main() {
-    int a, b, c;
-    cin >> a >> b >> c;
-    if (a < 2 || b < 2 || c < 2) return 0;
-    cout << (a + b) % c << endl;
-    cout << ((a % c) + (b % c)) % c << endl;
-    cout << (a * b) % c << endl;
-    cout << ((a % c) * (b % c)) % c << endl;
+    int a, b, b1, b2, b3;
+    cin >> a >> b;
+    if (a <= 0 || a > 999 || b <= 0 || b > 999) return 0;
+    b1 = b / 100;
+    b2 = b % 100;
+    b3 = b2 % 10;
+    b2 = b2 / 10;
+    cout << a * b3 << endl;
+    cout << a * b2 << endl;
+    cout << a * b1 << endl;
+    cout << a * b;
 }
