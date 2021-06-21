1527223446d824de789de864309ca9dd01d525ec
finale225 <finale2255@naver.com>
2021-06-22 01:30:59 +0900

    remainder

--- main.cpp
+++ main.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 int main() {
-    int a, b;
-    cin >> a >> b;
-    if (a <= 0 || b <= 0) return 0;
-    cout << a+b << endl;
-    cout << a-b << endl;
-    cout << a*b << endl;
-    cout << a/b << endl;
-    cout << a%b;
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a < 2 || b < 2 || c < 2) return 0;
+    cout << (a + b) % c << endl;
+    cout << ((a % c) + (b % c)) % c << endl;
+    cout << (a * b) % c << endl;
+    cout << ((a % c) * (b % c)) % c << endl;
 }
